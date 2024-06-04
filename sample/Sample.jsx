d319903087e100d91dea583f4197a5ceee8fbaed
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2024-06-05 01:22:00 +1000

    Update sample app

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -5,6 +5,47 @@ import Clock from '../src/Clock';
 import '../src/Clock.less';
 import './Sample.less';
 
+function OldClock(props) {
+  return (
+    <Clock
+      hourHandWidth={5}
+      minuteHandWidth={3}
+      renderNumbers={true}
+      secondHandLength={70}
+      secondHandWidth={2}
+      size={500}
+      {...props}
+    />
+  );
+}
+
+function NewClock(props) {
+  return (
+    <Clock
+      hourHandLength={30}
+      hourHandWidth={8}
+      hourMarksLength={30}
+      hourMarksWidth={4}
+      hourHandOppositeLength={0}
+      minuteHandLength={50}
+      minuteHandWidth={7}
+      minuteHandOppositeLength={0}
+      newUI={true}
+      renderMinuteMarks={false}
+      renderNumbers={true}
+      secondHandLength={88}
+      secondHandWidth={7}
+      size={500}
+      {...props}
+    />
+  );
+}
+
+function ClockWrapper({ newUI, ...props }) {
+  return newUI ? <NewClock {...props} /> : <OldClock {...props} />;
+  
+}
+
 export default function Sample() {
   const [value, onChange] = useState(new Date());
 
@@ -20,23 +61,7 @@ export default function Sample() {
       </header>
       <div className="Sample__container">
         <main className="Sample__container__content">
-          <Clock
-            hourHandLength={30}
-            hourHandWidth={8}
-            hourMarksLength={30}
-            hourMarksWidth={4}
-            hourHandOppositeLength={0}
-            minuteHandLength={50}
-            minuteHandWidth={7}
-            minuteHandOppositeLength={0}
-            newUI={true}
-            renderMinuteMarks={false}
-            renderNumbers={true}
-            secondHandLength={88}
-            secondHandWidth={7}
-            size={500}
-            value={value}
-          />
+          <ClockWrapper newUI={true} value={value} />
         </main>
       </div>
     </div>
